c6cd6c2b55f91722168d32009e94fd52aa0ab55a
Darko Ilic <darko.ilic@gmail.com>
2012-10-05 13:15:19 -0700

    Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonTracker::Application.config.secret_token = 'a7d299fc7409a8da05bb5c5ef572614e295042fc5340b4b5b3f80d90b9c6080010fff45db61c9415dfff8004b4416da85d88e63469ab7ac732dc9569a7227c29'
